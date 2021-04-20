7700feb3623f1478264b788e2a11becd49c83b93
MMarcos <mauricio.marcos@castgroup.com.br>
2021-04-19 21:38:00 -0300

    Finalizada a implementação da paginação.

--- playlist-frontend/src/app/shared/service/video.service.ts
+++ playlist-frontend/src/app/shared/service/video.service.ts
@@ -8,48 +8,53 @@ import { Video } from '../model/video.model';
   providedIn: 'root'
 })
 export class VideoService {
-  baseURL = 'https://localhost:44346/api/Video';
-
-  criar = '/criar';
-  atualizar = '/atualizar/';
-  excluir = '/excluir/'
-  buscaPaginado = '/buscarPaginado';
-  buscarPorId = '/buscarPorId';
-
-
-  httpOptions = {
+  private baseURL = 'https://localhost:44346/api/Video';
+  private criar = '/criar';
+  private atualizar = '/atualizar/';
+  private excluir = '/excluir/'
+  private buscaPaginado = '/buscarPaginado';
+  private buscarPorId = '/buscarPorId';
+  private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
   }
-  constructor(private httpClient: HttpClient) { }
 
-  public getVideosPagineted(flag: boolean): Observable<ResponsePageable>{
-    let endpoint = (this.baseURL + this.buscaPaginado + '?visualizado=' + flag);
-    
+  constructor(
+    private httpClient: HttpClient
+    ) { }
+
+  public getVideosPagineted(pageNumber: any, pageSize: any, visualizado: boolean): Observable<ResponsePageable>{    
+    let endpoint = `${this.baseURL}${this.buscaPaginado}?visualizado=${visualizado}`;
+
+    if (pageNumber != null && pageSize != null)
+      endpoint = `${this.baseURL}${this.buscaPaginado}/${pageNumber}/${pageSize}?visualizado=${visualizado}`;
+    else if (pageNumber != null)
+      endpoint = `${this.baseURL}${this.buscaPaginado}/${pageNumber}?visualizado=${visualizado}`;
+
     console.log(endpoint);
-    return this.httpClient.get<ResponsePageable>(endpoint)
+    return this.httpClient.get<ResponsePageable>(endpoint, this.httpOptions)
   }
 
   public getById(id: string): Observable<Video>{
     let endpoint = (this.baseURL + this.buscarPorId + id);
 
     console.log(endpoint);
-    return this.httpClient.get<Video>(endpoint)
+    return this.httpClient.get<Video>(endpoint, this.httpOptions)
   }
 
   public postVideo(video: Video): Observable<Video>{
-    let endpoint = this.baseURL + this.criar
+    let endpoint = (this.baseURL + this.criar);
     return this.httpClient.post<Video>(endpoint, video, this.httpOptions);
   };
 
   public putVideo(id: string, video: Video): Observable<Video>{
-    let endpoint = this.baseURL + this.atualizar + id;
+    let endpoint = (this.baseURL + this.atualizar + id);
     return this.httpClient.put<Video>(endpoint, video, this.httpOptions);
   };
 
   public delete(id: string): Observable<Video>{
-    let endpoint = this.baseURL + this.excluir + id;
+    let endpoint = (this.baseURL + this.excluir + id);
     return this.httpClient.delete<Video>(endpoint, this.httpOptions);
   }
 }
